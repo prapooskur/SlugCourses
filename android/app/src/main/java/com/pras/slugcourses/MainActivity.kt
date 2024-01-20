f7f6d4c2dfd668c370a1a2c26fd97d6c67eff260
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-01-19 23:42:23 -0800

    added edge to edge, set up file structure

--- android/app/src/main/java/com/pras/slugcourses/MainActivity.kt
+++ android/app/src/main/java/com/pras/slugcourses/MainActivity.kt
@@ -2,12 +2,17 @@ package com.pras.slugcourses
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.pras.slugcourses.api.APIResponse
 import com.pras.slugcourses.api.Course
 import com.pras.slugcourses.api.SupabaseQuery
 import com.pras.slugcourses.ui.theme.SlugCoursesTheme
@@ -23,7 +27,25 @@ import com.pras.slugcourses.ui.theme.SlugCoursesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
+            val darkTheme = isSystemInDarkTheme()
+            DisposableEffect(isSystemInDarkTheme()) {
+                val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+                val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { darkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { darkTheme },
+                )
+                onDispose {}
+            }
+
             SlugCoursesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -31,7 +53,13 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     var response by remember { mutableStateOf(listOf<Course>()) }
-                    Greeting("Android")
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        items(response.size) { course ->
+                            Text(text = response[course].department+" "+response[course].course_number)
+                        }
+                    }
                     LaunchedEffect(Unit) {
                         response = SupabaseQuery(2240)
                     }
