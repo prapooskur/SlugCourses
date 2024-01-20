8b5f7a703eaf180283f48edd185d8f364f118496
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-01-20 12:08:25 -0800

    now with better looking icons and 33% less crashes

--- android/app/src/main/java/com/pras/slugcourses/HomeScreen.kt
+++ android/app/src/main/java/com/pras/slugcourses/HomeScreen.kt
@@ -2,30 +2,44 @@ package com.pras.slugcourses
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import com.pras.slugcourses.api.Course
 import com.pras.slugcourses.api.SupabaseQuery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun HomeScreen(navController: NavController) {
     var response by remember { mutableStateOf(listOf<Course>()) }
+    var dataLoaded by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        items(response.size) { course ->
-            Text(text = response[course].department+" "+response[course].course_number)
+        if (dataLoaded) {
+            items(response.size) { course ->
+                Text(text = response[course].department + " " + response[course].course_number)
+            }
+        } else {
+            item {
+                CircularProgressIndicator()
+            }
         }
     }
     LaunchedEffect(Unit) {
-        response = SupabaseQuery(2240)
+        withContext(Dispatchers.IO) {
+            response = SupabaseQuery(2240)
+            dataLoaded = true
+        }
     }
 }
--- android/app/src/main/java/com/pras/slugcourses/MainActivity.kt
+++ android/app/src/main/java/com/pras/slugcourses/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pras.slugcourses
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
@@ -14,7 +15,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -28,12 +30,16 @@ import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.pras.slugcourses.ui.theme.SlugCoursesTheme
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -87,12 +93,36 @@ fun GreetingPreview() {
     }
 }
 
+data class BottomNavigationItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector,
+    val selectedIcon: ImageVector,
+    val iconDescription: String
+)
+
 const val DELAYTIME: Float = 350F
 const val FADETIME: Float = 200F
 @Composable
 fun Init(startDestination: String) {
     val navController = rememberNavController()
     val index = remember { mutableIntStateOf(0) }
+    val itemList = listOf(
+        BottomNavigationItem(
+            name = "Home",
+            route = "home",
+            selectedIcon =  Icons.Filled.Home,
+            icon = Icons.Outlined.Home,
+            iconDescription = "Home"
+        ),
+        BottomNavigationItem(
+            name = "Chat",
+            route = "chat",
+            selectedIcon = ImageVector.vectorResource(R.drawable.chat_filled),
+            icon = ImageVector.vectorResource(R.drawable.chat_outlined),
+            iconDescription = "Favorite"
+        )
+    )
 
 
     Scaffold(
@@ -123,22 +153,37 @@ fun Init(startDestination: String) {
             }
         },
         bottomBar = {
-            BottomNavigationBar(navController, listOf("home", "chat"), index)
+            BottomNavigationBar(navController, itemList, index)
         }
     )
 }
 
 @Composable
-fun BottomNavigationBar(navController: NavController, items: List<String>, selectedItem: MutableIntState) {
+fun BottomNavigationBar(navController: NavController, items: List<BottomNavigationItem>, selectedItem: MutableIntState) {
     NavigationBar {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                label = { Text(item) },
+                icon = {
+                    if (selectedItem.intValue == index) {
+                        Icon(
+                            imageVector = item.selectedIcon,
+                            contentDescription = item.iconDescription
+                        )
+                    } else {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.iconDescription
+                        )
+                    }
+                },
+                label = { Text(item.name) },
                 selected = selectedItem.intValue == index,
                 onClick = {
                     selectedItem.intValue = index
-                    navController.navigate(item) {
+
+                    Log.d(TAG, index.toString())
+                    Log.d(TAG, selectedItem.intValue.toString())
+                    navController.navigate(item.route) {
                         popUpTo(navController.graph.startDestinationId)
                         launchSingleTop = true
                     }
