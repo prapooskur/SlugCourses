14648e945f5ab66e49b134938b4fde12d48c3d60
iconsumeplutonium <uzymc242@outlook.com>
2024-01-19 20:32:33 -0800

    add scraper python code (again)

---
+++ backend/scraper.py
@@ -0,0 +1,63 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+URL = "https://pisa.ucsc.edu/class_search/index.php"
+MAX_RESULTS = "2000"
+
+
+def queryPisa():
+    info = {
+        "action": "results",
+        "binds[:term]": "2240",
+        "binds[:reg_status]": "all",
+        "binds[:subject]": "",
+        "binds[:catalog_nbr_op]": "",
+        "binds[:catalog_nbr]": "",
+        "binds[:title]": "",
+        "binds[:instr_name_op]": "=",
+        "binds[:instructor]": "",
+        "binds[:ge]": "",
+        "binds[:crse_units_op]": "=",
+        "binds[:crse_units_from]": "",
+        "binds[:crse_units_to]": "",
+        "binds[:crse_units_exact]": "",
+        "binds[:day]": "",
+        "binds[:times]": "",
+        "binds[:acad_career]": "",
+        "binds[:asynch]": "A",
+        "binds[:hybrid]": "H",
+        "binds[:synch]": "S",
+        "binds[:person]": "P",
+        "rec_start": "0",
+        "rec_dur": MAX_RESULTS
+    }
+
+    response = requests.post(URL, data=info)
+    doc = BeautifulSoup(response.text, 'html.parser')
+
+    sections = []
+    for panel in doc.select(".panel.panel-default.row"):
+        locations = len(panel.select(".fa-location-arrow"))
+        summer = len(panel.select(".fa-calendar")) != 0
+
+        section = {
+            "name": panel.select("a")[0].text.replace("\xa0\xa0\xa0", ' ').strip(),
+            "id": int(panel.select("div > a")[0].text) if panel.select("div > a")[0].text.isdigit() else 0,
+            "instructor": panel.select(".col-xs-6:nth-child(2)")[0].text.split(": ")[1].replace(",", ", ").strip(),
+            "location": panel.select(".col-xs-6:nth-child(1)")[1].text.split(": ", 1)[1].strip(),
+            "time": panel.select(".col-xs-6:nth-child(2)")[1].text.split(": ")[1].strip() if len(panel.select(".col-xs-6:nth-child(2)")[1].text.split(": ")) > 1 else "None",
+            "location2": panel.select(".col-xs-6:nth-child(3)")[0].text.split(": ", 1)[1].strip() if locations > 1 else "None",
+            "time2": panel.select(".col-xs-6:nth-child(4)")[0].text.split(": ")[1].strip() if locations > 1 else "None",
+            "count": panel.select(".col-xs-6:nth-child({})".format(5 if summer else 4))[locations - 1].text.strip(),
+            "mode": panel.select("b")[0].text.strip(),
+            "summerSession": panel.select(".col-xs-6:nth-child(4)")[0].text.split(": ")[1].strip() if summer else "None",
+            "url": panel.select("a")[0]['href'].strip(),
+            "status": panel.select("h2 .sr-only")[0].text.strip()
+        }
+        sections.append(section)
+
+
+    return sections
+
+
+#queryPisa()
