efe0fc8704ae480b28502decad4445b25f6b7d04
iconsumeplutonium <uzymc242@outlook.com>
2024-01-20 20:54:31 -0800

    Annihilate pipeline. Remake with haystack functions. Convert Gemini response to streaming API. This commit goes so hard. The best commit.

--- backend/recommendation.py
+++ backend/recommendation.py
@@ -1,5 +1,5 @@
-import pickle, os, re, asyncio
-from haystack import Pipeline
+import pickle, os, re, asyncio, time
+#from haystack import Pipeline
 from haystack.components.joiners.document_joiner import DocumentJoiner
 from haystack.components.retrievers.in_memory import InMemoryBM25Retriever, InMemoryEmbeddingRetriever
 from haystack.components.embedders import SentenceTransformersTextEmbedder
@@ -8,12 +8,17 @@ from haystack.components.builders.prompt_builder import PromptBuilder
 from supabase import create_client, client
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+import google.generativeai as genai
+
 
 
 # create app and load .env
 classRecommender = FastAPI()
+
+# load API keys from .env
 load_dotenv()
-geminiKey = os.getenv("KEY")
+geminiKey = os.getenv("GEMINIKEY")
 supaKey = os.getenv("SUPAKEY")
 supaUrl = os.getenv("SUPAURL")
 
@@ -31,7 +36,7 @@ If you recommend graduate level courses, separate undergraduate and graduate lev
 If there are no graduate level courses in your recommendation, keep the courses in one list.
 Your response must be formatted in a bullet point list with a blank line after each bullet point.
 You must include the class's code and full name in your response.
-If you choose to have bullet point descriptions for each class, make sure each line is in a spartan tone.
+Write a a couple brief bullet points per class you recommend. Include a brief summary of prerequisites and any enrollment restrictions.
 '''
 
 
@@ -40,47 +45,67 @@ document_file = open("backend/updatedclasses", mode="rb")
 document_store = pickle.load(document_file)
 document_file.close()
 
-
-# set up embedders and retrievers
+# create text embeddings for instruction
+# textEmbeddings -> a list of floats
 query_instruction = "Represent this sentence for searching relevant passages:"
-text_embedder = SentenceTransformersTextEmbedder(model="BAAI/bge-large-en-v1.5", prefix = query_instruction)
-embedding_retriever = InMemoryEmbeddingRetriever(document_store = document_store)
+text_embedder = SentenceTransformersTextEmbedder(model="BAAI/bge-large-en-v1.5", prefix=query_instruction)
+text_embedder.warm_up()
 
+
+# find documents based on documents (vector based)
+# embeddingDocs -> dictionary of documents
+embedding_retriever = InMemoryEmbeddingRetriever(document_store=document_store)
+
+
+# find documents based on keyword matches
+# bm25Docs -> dictionary of documents
 bm25_retriever = InMemoryBM25Retriever(document_store=document_store)
+
+
+# merge embeddingDocs and bm25Docs via reciprocal rank fusion method
+# mergedDocs -> list of documents 
 document_joiner = DocumentJoiner(join_mode="reciprocal_rank_fusion")
-prompt_builder = PromptBuilder(template=prompt_template)
-llm = GoogleAIGeminiGenerator(model="gemini-pro", api_key=geminiKey)
 
 
-# set up pipeline
-query_pipeline = Pipeline()
-query_pipeline.add_component("text_embedder", text_embedder)
-query_pipeline.add_component("embedding_retriever", embedding_retriever)
-query_pipeline.add_component("bm25_retriever", bm25_retriever)
-query_pipeline.add_component("joiner", document_joiner)
-query_pipeline.add_component("prompt_builder", prompt_builder)
-query_pipeline.add_component("llm", llm)
-
-query_pipeline.connect("text_embedder.embedding", "embedding_retriever.query_embedding")
-query_pipeline.connect("embedding_retriever", "joiner")
-query_pipeline.connect("bm25_retriever", "joiner")
-query_pipeline.connect("joiner", "prompt_builder.documents")
-query_pipeline.connect("prompt_builder", "llm")
+# create LLM
+genai.configure(api_key=geminiKey)
+model = genai.GenerativeModel('gemini-pro')
 
 
+# uvicorn backend.recommendation:classRecommender
+@classRecommender.get("/")
+async def get_stream(userInput : str):
 
-async def GetRecommendations(userPrompt : str, query_pipeline : Pipeline) -> str:
 
-    print(userPrompt)
-    results = query_pipeline.run(
-        {
-            "bm25_retriever": {"query": userPrompt},
-            "text_embedder":{"text": userPrompt},
-            "prompt_builder": {"question": userPrompt},
-        }
-    )
+    # generate text embeddings based on user input
+    textEmbeddings = text_embedder.run(userInput)
 
-    recommendations = str(results["llm"]["answers"][0]).replace("\\n", "\n")
+    # create embeddings (vector based document search)
+    embeddingDocs = embedding_retriever.run(query_embedding=textEmbeddings['embedding'])
+
+    # keyword based document search
+    bm25Docs = bm25_retriever.run(query=userInput)
+
+    # merge vector-based docs and keyword-based docs
+    mergedDocs = document_joiner.run([bm25Docs["documents"], embeddingDocs["documents"]])
+
+
+    # put user input and list of documents into the prompt template
+    # finalizedPrompt -> string
+    prompt_builder = PromptBuilder(template=prompt_template)
+    finalizedPrompt = prompt_builder.run(documents=mergedDocs["documents"], question=userInput)["prompt"]
+
+    response = model.generate_content(finalizedPrompt, stream=True)
+
+    def stream_data():
+        for chunk in response:
+            yield chunk.text
+            time.sleep(1)
+
+    return StreamingResponse(stream_data())
+
+
+    # recommendations = str(results["llm"]["answers"][0]).replace("\\n", "\n")
 
     # classRegex = re.compile("[A-Z]{3,4}\s\d{1,3}[A-Z]?")
     # matches = list(set(re.findall(classRegex, recommendations)))
@@ -104,17 +129,4 @@ async def GetRecommendations(userPrompt : str, query_pipeline : Pipeline) -> str
     #     except:
     #         print(f"Warning: {department} {catalogNum} does not exist in database.")
     #     else:
-    #         recommendations = recommendations.replace(match, f"[{department} {catalogNum}]({classID})")
-
-
-
-    #print(response)
-
-    return recommendations
-
-
-
-@classRecommender.get("/")
-def response(prompt : str):
-    llmResponse = asyncio.run(GetRecommendations(prompt, query_pipeline))
-    return {llmResponse}
+    #         recommendations = recommendations.replace(match, f"[{department} {catalogNum}]({classID})"
