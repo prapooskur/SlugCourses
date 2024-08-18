d9892e1c764d6d2758c4f1e482cbd293cc87c890
prapooskur <>
2024-08-17 20:18:34 -0700

    deleted old scraper and replaced with scraper_parallel

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,17 +1,103 @@
-import requests, json, sys, os, re
+import requests, json, sys, os, re, concurrent.futures, time
+from bs4 import BeautifulSoup, SoupStrainer
+from concurrent.futures import ThreadPoolExecutor
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
+from multiprocessing import Pool
 from supabase import create_client, Client
 from tqdm import tqdm # optional, shows progress bar
-import concurrent.futures
 
 URL = "https://pisa.ucsc.edu/class_search/index.php"
 PISA_API = "https://my.ucsc.edu/PSIGW/RESTListeningConnector/PSFT_CSPRD/SCX_CLASS_DETAIL.v1/"
 MAX_RESULTS = "2000"
 
-def queryPisa(term: str, gened: bool = False) -> list[dict]:
-    load_dotenv()
 
+# takes in a single panel from BS4 and parses it into a dictionary
+# delegate used in the multithreading in queryPisa
+def parseSinglePanel(panel, term: str, detailed: bool) -> dict:
+
+    locations = len(panel.select(".fa-location-arrow"))
+    summer = len(panel.select(".fa-calendar")) != 0
+
+    name = panel.select("a")[0].text.replace("\xa0\xa0\xa0", ' ').strip()
+
+    course = name.split(" - ")
+    primary = course[0].split(" ", maxsplit=1)
+    secondary = course[1].split(" ", maxsplit=1)
+
+    department = primary[0].strip()
+    full_course_number = primary[1].strip()
+    course_number = full_course_number
+
+    course_match = re.match(r'(\d+)(\D*)', full_course_number)
+    course_letter = " "
+
+    if course_match:
+        course_number = course_match.group(1)
+        course_letter = course_match.group(2)
+
+    section_number = secondary[0].strip()
+    description = secondary[1].strip()
+
+    # these elements are accessed multiple times
+    # store them in variables to reduce calls to select() (which is slow)
+    panelNthChild2 = panel.select(".col-xs-6:nth-child(2)")
+    panelNthChild4 = panel.select(".col-xs-6:nth-child(4)")[0]  
+    panelDivA = panel.select("div > a")[0].text
+
+    # edge cases
+    #some classes have three locations - this causes enrollment to break if left unhandled
+    #in summer, enrollment is always the only itme in the array
+
+    if summer:
+        enrolled_index = 0
+    else:
+        enrolled_index = min(locations-1, 1)
+
+    id = int(panelDivA) if panelDivA.isdigit() else 0
+    combined_id = str(term)+"_"+str(id)
+
+    section = {
+        "id": combined_id,
+        "term": term,
+        "department": department,
+        "course_number": course_number,
+        "course_letter": course_letter,
+        "section_number": section_number,
+        "short_name": description,
+        "instructor": panelNthChild2[0].text.split(": ")[1].replace(",", ", ").strip(),
+        "location": panel.select(".col-xs-6:nth-child(1)")[1].text.split(": ", 1)[1].strip(),
+        "time": panelNthChild2[1].text.split(": ")[1].strip() if len(panelNthChild2[1].text.split(": ")) > 1 else "None",
+        "alt_location": panel.select(".col-xs-6:nth-child(3)")[0].text.split(": ", 1)[1].strip() if locations > 1 else "None",
+        "alt_time": panelNthChild4.text.split(": ")[1].strip() if locations > 1 else "None",
+        "enrolled": panel.select(".col-xs-6:nth-child({})".format(5 if summer else 4))[enrolled_index].text.strip(),
+        "type": panel.select("b")[0].text.strip(),
+        "summer_session": panelNthChild4.text.split(": ")[1].strip() if summer else "None",
+        "url": panel.select("a")[0]['href'].strip(),
+        "status": panel.select("h2 .sr-only")[0].text.strip()
+    }
+
+    try:
+        int(section["course_number"])
+    except ValueError:
+        # print("invalid course_number", section["course_number"])
+        # only a few classes do this and they're all listed as "external", so skip if it's not valid so the upload doesn't fail.
+        return
+
+    if detailed:
+        pisaApiResponse = json.loads(requests.get(PISA_API + f'{term}/{id}').text)
+        if "primary_section" in pisaApiResponse:
+            if "gened" in pisaApiResponse["primary_section"]:
+                section["gen_ed"] = pisaApiResponse["primary_section"]["gened"]
+            if "title_long" in pisaApiResponse["primary_section"]:
+                section["name"] = pisaApiResponse["primary_section"]["title_long"]
+            if "description" in pisaApiResponse["primary_section"]:
+                section["description"] = pisaApiResponse["primary_section"]["description"]
+
+    return section
+
+
+
+def queryPisa(term: str, detailed: bool = False) -> list[dict]:
     info = {
         "action": "results",
         "binds[:term]": term,
@@ -37,113 +123,59 @@ def queryPisa(term: str, gened: bool = False) -> list[dict]:
         "rec_start": "0",
         "rec_dur": MAX_RESULTS
     }
-
-    response = requests.post(URL, data=info)
-    doc = BeautifulSoup(response.text, 'html.parser')
-
     sections = []
 
-    # debug - disable tqdm
-    #for panel in doc.select(".panel.panel-default.row"):
-    for panel in tqdm(doc.select(".panel.panel-default.row")):
 
-       
+    response = requests.post(URL, data=info)
+    strainedSoup = SoupStrainer(class_="panel panel-default row") # doesnt help much tbh
+    doc = BeautifulSoup(response.content, features='lxml', parse_only=strainedSoup)
+    #print(f"Time to make BS4 object: {time.time() - startTime} seconds")
 
-        locations = len(panel.select(".fa-location-arrow"))
-        summer = len(panel.select(".fa-calendar")) != 0
-
-        name = panel.select("a")[0].text.replace("\xa0\xa0\xa0", ' ').strip()
-
-        course = name.split(" - ")
-        primary = course[0].split(" ", maxsplit=1)
-        secondary = course[1].split(" ", maxsplit=1)
-
-        department = primary[0].strip()
-        full_course_number = primary[1].strip()
-        course_number = full_course_number
-        #course_number, course_letter = re.match(r'(\d+)([a-zA-Z]*)', course_number).groups() if re.match(r'(\d+)([a-zA-Z]*)', course_number) else (course_number, '')
-
-        course_match = re.match(r'(\d+)(\D*)', full_course_number)
-        course_letter = " "
-
-        if course_match:
-            course_number = course_match.group(1)
-            course_letter = course_match.group(2)
-
-        section_number = secondary[0].strip()
-        description = secondary[1].strip()
-
-        # edge cases
-        #some classes have three locations - this causes enrollment to break if left unhandled
-        #in summer, enrollment is always the only itme in the array
-
-        if summer:
-            enrolled_index = 0
-        else:
-            enrolled_index = min(locations-1, 1)
-        
-        id = int(panel.select("div > a")[0].text) if panel.select("div > a")[0].text.isdigit() else 0,
-        combined_id = str(term)+"_"+str(id)
-
-        section = {
-            "id": combined_id,
-            "term": term,
-            "department": department,
-            "course_number": course_number,
-            "course_letter": course_letter,
-            "section_number": section_number,
-            "short_name": description,
-            "instructor": panel.select(".col-xs-6:nth-child(2)")[0].text.split(": ")[1].replace(",", ", ").strip(),
-            "location": panel.select(".col-xs-6:nth-child(1)")[1].text.split(": ", 1)[1].strip(),
-            "time": panel.select(".col-xs-6:nth-child(2)")[1].text.split(": ")[1].strip() if len(panel.select(".col-xs-6:nth-child(2)")[1].text.split(": ")) > 1 else "None",
-            "alt_location": panel.select(".col-xs-6:nth-child(3)")[0].text.split(": ", 1)[1].strip() if locations > 1 else "None",
-            "alt_time": panel.select(".col-xs-6:nth-child(4)")[0].text.split(": ")[1].strip() if locations > 1 else "None",
-            "enrolled": panel.select(".col-xs-6:nth-child({})".format(5 if summer else 4))[enrolled_index].text.strip(),
-            "type": panel.select("b")[0].text.strip(),
-            "summer_session": panel.select(".col-xs-6:nth-child(4)")[0].text.split(": ")[1].strip() if summer else "None",
-            "url": panel.select("a")[0]['href'].strip(),
-            "status": panel.select("h2 .sr-only")[0].text.strip()
-        }
-
-        if gened:
-            #pisaApiResponse = json.loads(requests.get(PISA_API + f'{term}/{section["id"]}').text)
-            #section["gen_ed"] = pisaApiResponse["primary_section"]["gened"]
-            pisaApiResponse = json.loads(requests.get(PISA_API + f'{term}/{section["id"]}').text)
-            if "primary_section" in pisaApiResponse and "gened" in pisaApiResponse["primary_section"]:
-                section["gen_ed"] = pisaApiResponse["primary_section"]["gened"]
-            if "primary_section" in pisaApiResponse and "title_long" in pisaApiResponse["primary_section"]:
-                section["name"] = pisaApiResponse["primary_section"]["title_long"]
-
-        sections.append(section)
+    panels = doc.select(".panel.panel-default.row")
     
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
+    with ThreadPoolExecutor() as executor:
+        future_to_section = {executor.submit(parseSinglePanel, panel, term, detailed): panel for panel in panels}
+        for future in concurrent.futures.as_completed(future_to_section):
+            section = future.result()
+            # if the course number isn't a number the function returns none, so a check is needed
+            if section:
+                sections.append(section)
 
-    supabase.table("courses").upsert(sections).execute()
+    return sections
             
 
-        
-'''
-match(len(sys.argv)):
-    case 1:
-        print("Run with args pls")
-    case 2:
-        queryPisa(sys.argv[1], False)
-    case _:
-        if (sys.argv[2] == "True"):
-            queryPisa(sys.argv[1], True)
-        else:
-            queryPisa(sys.argv[1], False)
-'''
+term_list = [2248, 2244, 2242, 2240, 2238, 2234, 2232, 2230, 2228, 2224]
 
-#term_list = [2242, 2240, 2238, 2234, 2232, 2230, 2228, 2224]
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
-#with concurrent.futures.ThreadPoolExecutor() as executor:
-#    results = list(executor.map(lambda term: queryPisa(str(term), False), term_list))
 
-queryPisa("2242", False)
-#queryPisa("2240")
 
-#for term in term_list:
-#    queryPisa(str(term))
+if len(sys.argv) > 1:
+    do_detail = "-g" in sys.argv
+    if do_detail:
+        print("getting detailed info")
+    if "-a" in sys.argv:
+        print("getting every term")
+        for term in term_list:
+            print("scraping "+str(term))
+            sections = queryPisa(term, do_detail)
+            supabase.table("courses").upsert(sections).execute()
+    else:
+        if int(sys.argv[1]) in term_list:
+            print("scraping specific term: "+str(sys.argv[1]))
+            sections = queryPisa(sys.argv[1], do_detail)
+            supabase.table("courses").upsert(sections).execute()
+        elif do_detail:
+            term = term_list[0]
+            print("scraping latest term: "+str(term))
+            sections = queryPisa(term, True)
+            supabase.table("courses").upsert(sections).execute()
+else:
+    terms = [2244, 2248]
+    print("scraping "+str(terms))
+    for term in terms:
+        sections = queryPisa(term, False)
+        supabase.table("courses").upsert(sections).execute()
