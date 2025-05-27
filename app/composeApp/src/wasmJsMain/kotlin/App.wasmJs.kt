c9731f5f90df57b67b11193dd14a3ca294f1ed42
Prasiddh Pooskur <61389919+prapooskur@users.noreply.github.com>
2025-05-27 16:47:13 -0700

    maybe fix wasm screen size check

--- app/composeApp/src/wasmJsMain/kotlin/App.wasmJs.kt
+++ app/composeApp/src/wasmJsMain/kotlin/App.wasmJs.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.Dispatchers
 
 @Composable
 actual fun GetScreenSize(): IntSize {
-    return LocalWindowInfo.current.containerSize
+    // return LocalWindowInfo.current.containerSize
+    val intSize = LocalWindowInfo.current.containerSize
+    return IntSize(intSize.width.toDp(), intSize.height.toDp())
 }
 
 actual val ioDispatcher: CoroutineDispatcher = Dispatchers.Default
